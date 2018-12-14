ca84e90acb26fd10300390f31d6c14edd80de117
unknown <maher.alkendi2@gmail.com>
2018-12-14 21:40:09 +0000

    created a new api that connects to a local sql db

--- index.js
+++ index.js
@@ -1,34 +1,40 @@
 const express = require('express');
-const path = require('path');
 const bodyParser = require('body-parser');
-const ejs = require('ejs');
-const pg = require('pg');
+const mysql      = require('mysql');
+const morgan = require('morgan');
+require('dotenv').config(); // configure .env file
+
+// connect to local sql db
+var connection = mysql.createConnection({
+  host     : process.env.DB_HOST,
+  user     : process.env.DB_USER,
+  password : process.env.DB_PASSWORD,
+  database : process.env.DB_NAME
+});
 
 // initialize express
 const app = express();
 
-// setup db connection string
-const connect = 'postgresql://username:password@database.server.com:3211/mydb';
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// Set the views folder to be our template source
-app.set('views', __dirname + '/views');
-
-// Set public folder
-app.use(express.static(path.join(__dirname, 'public')));
+// initialize morgan to get logs
+app.use(morgan('dev'));
 
 // body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// routes
-app.get('/', (req, res) => {
-    res.render('index');
-})
+// GET route
+app.get('/books', (req, res) => {
+  connection.connect();
+
+  connection.query('SELECT * FROM booklist', function (error, results) {
+    if (error) throw error;
+    res.json({message:results[0]})
+  });
+
+  connection.end();
+});
 
 // server
 app.listen(3000, () => {
     console.log('Server started on port 3000...');
-})
+});
